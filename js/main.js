a5cdcd9d2a936bf9f8d8ac6e5cc80c15388a7742
prabeen <prabeenprabu@gmail.com>
2022-08-23 23:13:07 -0400

    variable declared

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// ----------------getting elements----------------
+let hours = document.getElementById("hours");
+let seconds = document.getElementById("secs");
+let minutes = document.getElementById("mins");
+let ampm = document.getElementById("ampm");
+
